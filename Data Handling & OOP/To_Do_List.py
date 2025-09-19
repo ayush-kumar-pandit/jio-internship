9dca8158e2328cbfdf9962547d0108ce641a0c35
Ayush <ayushpandit672@gmail.com>
2025-09-19 12:36:20 +0530

    Data Handling & OOP UPDATED

--- Data Handling & OOP/To_Do_List.py
+++ Data Handling & OOP/To_Do_List.py
@@ -17,6 +17,10 @@ class task_manager():
     def add_task(self,id,description):
         self.obj.append(task(id,description))
 
+    def update_status(self,id):
+        for row in self.obj:
+            if row.dic['ID'] == id:
+                row.dic['Status'] = 'Completed'
 
     def list_task(self):
         for row in self.obj:
@@ -28,11 +32,15 @@ class task_manager():
             writer = csv.DictWriter(csv_file,fieldnames = ['ID','Description','Status'])
             for row in self.obj:
                 writer.writerow(row.dic)
-        print('Data saved as CSV')
+        print('Data saved as CSV!!')
 
     
-    def load_from_csv():
-        pass
+    def load_from_csv(self):
+        print('Data retrieved from CSV!!')
+        with open('To Do List.csv',newline = '')as csv_file:
+            reader = csv.DictReader(csv_file,fieldnames = ['ID','Description','Status'])
+            for row in reader:
+                print(row['ID'],row['Description'],row['Status'])
     
 
 
@@ -41,8 +49,18 @@ if __name__ == '__main__':
     TM = task_manager(1,'Install python 3.10+')
 
     TM.add_task(2,'Install Vs code')
-    TM.add_task(3,'Setup GitHUb Account')
+    TM.add_task(3,'Learn about Venv')
+    TM.add_task(4,'Setup Github account')
+    TM.add_task(5,'Master basic Git workflow')
+
 
     TM.list_task()
+
+    TM.update_status(1)
+    TM.update_status(2)
+    TM.update_status(3)
     TM.save_to_csv()
+    TM.load_from_csv()
+
+
         
